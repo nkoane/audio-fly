9c9b97bdd9afa033bebe718fa5cca3a294bcd1a2
lebogang nkoane <1555646+nkoane@users.noreply.github.com>
2024-06-29 15:53:27 +0200

    added: h,j,k, and l navigation of transcript

--- main.js
+++ main.js
@@ -7,24 +7,25 @@ window.addEventListener('DOMContentLoaded', (ev) => {
     const storyList = document.querySelector('main div ol');
 
     const offSetTime = data.words[0].start / 1000;
+    let parent;
 
     let gap = {
         start: 0,
         end: null,
     };
 
-    for (const [index, utterance] of data.utterances.entries()) {
+    for (const [utteranceIndex, utterance] of data.utterances.entries()) {
         const li = document.createElement('li');
         const dl = document.createElement('dl');
         const dt = document.createElement('dt');
         dt.innerText = utterance.speaker;
         const dd = document.createElement('dd');
-        for (const [index, word] of utterance.words.entries()) {
+        for (const [wordIndex, word] of utterance.words.entries()) {
             const span = document.createElement('span');
             span.textContent = word.text;
             span.setAttribute('data-start', word.start);
             span.setAttribute('data-end', word.end);
-            span.setAttribute('data-index', index);
+            span.setAttribute('data-index', wordIndex);
             dd.appendChild(span);
         }
         dl.appendChild(dt).appendChild(dd);
@@ -46,7 +47,7 @@ window.addEventListener('DOMContentLoaded', (ev) => {
     });
 
     document.addEventListener('keydown', (event) => {
-        if (event.code === 'KeyK') {
+        if (event.code === 'Space') {
             if (audio.paused) {
                 audio.play();
             } else {
@@ -54,12 +55,32 @@ window.addEventListener('DOMContentLoaded', (ev) => {
             }
         }
 
-        if (event.code === 'KeyJ') {
-            audio.currentTime -= 5;
+        if (event.code === 'KeyH') {
+            audio.currentTime -= 0.5;
         }
 
         if (event.code === 'KeyL') {
-            audio.currentTime += 5;
+            audio.currentTime += 0.5;
+        }
+
+        if (event.code === 'KeyK' || event.code === 'KeyJ') {
+            const speakerBlocks = storyList.querySelectorAll('dl');
+            const parentIndex = Array.from(speakerBlocks).findIndex((speakerBlock) => {
+                return speakerBlock.contains(parent);
+            });
+            if (parentIndex !== -1) {
+                let speakerBlockIndex = null;
+
+                if (event.code === 'KeyK') {
+                    speakerBlockIndex = parentIndex - 1;
+                } else {
+                    speakerBlockIndex = parentIndex + 1;
+                }
+
+                if (speakerBlockIndex < speakerBlocks.length && speakerBlockIndex >= 0) {
+                    audio.currentTime = Number.parseInt(speakerBlocks[speakerBlockIndex].querySelector('span').getAttribute('data-start')) / 1000;
+                }
+            }
         }
     });
 
@@ -69,30 +90,26 @@ window.addEventListener('DOMContentLoaded', (ev) => {
             gap.end = null;
         }
 
-        const word = findWordByTime(data.words, audio.currentTime * 1000);
+        const word = data.words.find((word) => {
+            return word.start <= audio.currentTime * 1000 && word.end >= audio.currentTime * 1000;
+        });
+
         const index = data.words.findIndex((word) => {
             return word.start <= audio.currentTime * 1000 && word.end >= audio.currentTime * 1000;
         });
 
         if (word != null && word !== undefined) {
             status.textContent = `${word.text}`;
-            const parent = spans[index].parentElement.parentElement.parentElement;
-            spans.forEach((span, spanIndex) => {
+            parent = spans[index].parentElement.parentElement.parentElement;
+            for (const span of spans) {
                 span.classList.remove('selected');
                 span.parentElement.parentElement.parentElement.classList.add('hide');
-            });
+            }
 
             spans[index].classList.add('selected');
             parent.classList.remove('hide');
-            console.log(parent);
         }
     });
-
-    function findWordByTime(words, time) {
-        return words.find((word) => {
-            return word.start <= time && word.end >= time;
-        });
-    }
 
     audio.addEventListener('loadedmetadata', () => {});
 });
